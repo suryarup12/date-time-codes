6e3b3880e1710e7d8a4b6d8edfef2b78dfb70728
suryarup12 <86052297+suryarup12@users.noreply.github.com>
2022-07-08 12:13:31 +0530

    Add files via upload

---
+++ date_time.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+    time_t t;   
+    time(&t);
+    printf("\nThis program has been writeen at (date and time): %s", ctime(&t));
+    return 0;
+}
